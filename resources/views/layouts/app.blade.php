bd02b904ac5805615be2a82f7406981fc9fb2206
Adrian Rodriguez <adrian.rodriguez7109@comunidad.unam.mx>
2019-04-28 09:45:54 -0500

    Google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,11 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-139192272-1"></script>
+<script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-139192272-1');</script>
+
 </head>
 <body>
     <div id="app">
